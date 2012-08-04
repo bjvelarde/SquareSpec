74670d6173def87430933e522cd1cd5b379b0d88
Benjie Velarde <bvelarde@gmail.com>
2012-08-05 01:09:38 +0800

    updated square.php

--- square.php
+++ square.php
@@ -7,21 +7,32 @@ use SquareSpec\SpecSubject as SpecSubject;
 
 $t1 = microtime(TRUE);
 
-$files = scandir(SPECS_DIR);
 echo "\n";
-foreach ($files as $file) {
-    if (preg_match('/^([a-z0-9_]+)\.specs\.php$/', $file, $matches)) {
-        include(SPECS_DIR . $matches[0]);
-    } 
+$ok = TRUE;
+if (!isset($argv[1])) {
+    $files = scandir(SPECS_DIR);
+    foreach ($files as $file) {
+        if (preg_match('/^([a-z0-9_]+)\.specs\.php$/', $file, $matches)) {
+            include(SPECS_DIR . $matches[0]);
+        } 
+    }
+} elseif (file_exists(SPECS_DIR . $argv[1] . '.specs.php')) {
+    include(SPECS_DIR . $argv[1] . '.specs.php');
+} else {
+    $ok = FALSE;
 }
-echo "\n";
-if (SpecSubject::$failures) {    
-    echo "Failed on: \n -" . implode("\n -", SpecSubject::$failures);
+if ($ok) {
     echo "\n";
+    if (SpecSubject::$failures) {    
+        echo "Failed on: \n -" . implode("\n -", SpecSubject::$failures);
+        echo "\n";
+    }
+    echo "\nFailures: " . count(SpecSubject::$failures) . "\n";
+    echo 'Success: ' . SpecSubject::$success . "\n";
+    echo 'Total: ' . SpecSubject::$total . "\n\n";
+    
+    echo 'Time Elapsed: ' . (microtime(TRUE) - $t1);
+} else {
+    echo "Can't find specs for $argv[1]";
 }
-echo "\nFailures: " . count(SpecSubject::$failures) . "\n";
-echo 'Success: ' . SpecSubject::$success . "\n";
-echo 'Total: ' . SpecSubject::$total . "\n\n";
-
-echo 'Time Elapsed: ' . (microtime(TRUE) - $t1);
 ?>
